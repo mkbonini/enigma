15c4a9785fd55b7d7e45ff33229afcdfff4ebf84
mkbonini <mkbonini@gmail.com>
2022-06-14 13:46:14 -0600

    Refactor code to move helper methods to module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,6 @@
 require 'date'
 require 'pry'
-require './modules/gettable'
+require './lib/modules/gettable'
 
 class Enigma
 
@@ -10,104 +10,41 @@ class Enigma
         @alphabet = ("a".."z").to_a << " "
     end
 
-    def encrypt(message, key = get_key, offset = get_offset)
+    def encrypt(message, key = get_key, offset = get_date)
         encrypted_message = ""
         shifts = get_shift(key, offset)
-        i = 0
-        message.downcase.each_char do |letter|
+        message.downcase.each_char.with_index do |letter, i|
             if @alphabet.include?(letter)
-                encrypted_message << get_letter(letter, shifts[i]) 
+                encrypted_message << get_letter(letter, shifts[i % 4]) 
             else
                 encrypted_message << letter
             end
-            i == 3 ? i = 0 : i += 1
         end
         return_hash = {encryption: encrypted_message, key: key, date: offset}
     end
 
-    def decrypt(message, key = get_key, offset = get_offset)
+    def decrypt(message, key = get_key, offset = get_date)
         decrypted_message = ""
         shifts = get_shift(key, offset)
-        i = 0
-        message.downcase.each_char do |letter|
+        message.downcase.each_char.with_index do |letter,i|
             if @alphabet.include?(letter) 
-                decrypted_message << get_decrypt_letter(letter, shifts[i])
+                decrypted_message << get_decrypt_letter(letter, shifts[i % 4])
             else
                 decrypted_message << letter
             end
-                i == 3 ? i = 0 : i += 1
         end
         return_hash = {decryption: decrypted_message, key: key, date: offset}
     end
 
-    def crack(message, offset = get_offset)  
+    def crack(message, offset = get_date)  
         shifts = get_crack_shifts(message)
         key = get_crack_key(shifts, offset)
         cracked = decrypt(message, key, offset)
         while cracked[:decryption][-4 .. -1] != " end"
-            puts "did the thing"
             shifts[0] += 27
             key =  get_crack_key(shifts, offset)
             cracked = decrypt(message, key, offset)
         end
         cracked
-    end
-
-    def get_crack_shifts(message)
-        shifts = []
-        given = message[-4 .. -1]
-        target = ' end'
-        given.downcase.each_char.with_index do |letter,i|
-            shift = @alphabet.index(letter) - @alphabet.index(target[i])
-            shift = shift + 27 if shift < 0
-            shifts <<  shift
-        end
-        corrected_shifts = get_corrected_shifts(message, shifts)
-    end
-
-    def get_corrected_shifts(message, shifts)
-        corrected_shifts = Array.new(4)
-        i = (message.length - 4) % 4
-        shifts.each do |shift|
-             corrected_shifts[i] = shift
-             i == 3 ? i = 0 : i += 1
-        end
-        corrected_shifts
-    end
-
-    def get_crack_key(shifts, offset)
-        test = offset
-        offset = (offset.to_i ** 2).to_s[-4 .. -1]
-        offsets = [offset[0].to_i, offset[1].to_i, offset[2].to_i, offset[3].to_i]
-        keys = []
-        shifts.map.with_index { |shift,i| shift - offsets[i] >= 0 ? keys << shift - offsets[i] : keys << (shift + 27) - offsets[i]}
-        p keys
-        original_keys = keys.dup
-        key_strings = get_key_strings(keys)
-        original_key_strings = key_strings.dup
-        key_strings[1 .. 4].each.with_index do |key_string,i|
-            while key_string[0] != key_strings[i][1]
-                keys[i + 1] += 27
-                if keys[i + 1] >= 100
-                    keys[i] += 27
-                    key_strings[i] = keys[i].to_s
-                    # binding.pry
-                    keys[i + 1] = original_keys[i + 1]
-                    key_strings[i + 1] = original_key_strings[i + 1] 
-                    # binding.pry 
-                end
-                key_string = keys[i + 1].to_s
-            end
-            key_strings[i+1] = key_string
-        end
-        key = key_strings[0] + key_strings[1][-1] + key_strings[2][-1] + key_strings[3][-1]
-    end
-
-    def get_key_strings(keys)
-        key_check = keys.map {|key| 
-            key_string = key.to_s
-            key_string.prepend("0") until key_string.length == 2
-            key_string
-        }
     end
 end
--- lib/modules/gettable.rb
+++ lib/modules/gettable.rb
@@ -1,6 +1,6 @@
   module Gettable
   
-    def get_offset
+    def get_date
         date = Date.today
         offset = date.strftime("%d") + date.strftime("%m") + date.strftime("%y") 
     end
@@ -11,12 +11,15 @@
        key
     end
 
-    def get_shift(key, offset)
-        offset = (offset.to_i ** 2).to_s[-4 .. -1]
-        keys = [key[0 .. 1].to_i, key[1 .. 2].to_i, key[2 .. 3].to_i, key[3 .. 4].to_i]
+    def get_offsets(date)
+        offset = (date.to_i ** 2).to_s[-4 .. -1]
         offsets = [offset[0].to_i, offset[1].to_i, offset[2].to_i, offset[3].to_i]
+    end
 
-        # shifts = {a: keys[0] + offsets[0], b: keys[1] + offsets[1], c: keys[2] + offsets[2], d: keys[3] + offsets[3]}
+    def get_shift(key, date)
+        keys = [key[0 .. 1].to_i, key[1 .. 2].to_i, key[2 .. 3].to_i, key[3 .. 4].to_i]
+        offsets = get_offsets(date)
+
         shifts = [ keys[0] + offsets[0], keys[1] + offsets[1], keys[2] + offsets[2], keys[3] + offsets[3] ]
     end
 
@@ -40,5 +43,58 @@
         end
 
         @alphabet[new_index]
+    end
+
+    def get_crack_shifts(message)
+        shifts = []
+        given = message[-4 .. -1]
+        target = ' end'
+        given.downcase.each_char.with_index do |letter,i|
+            shift = @alphabet.index(letter) - @alphabet.index(target[i])
+            shift = shift + 27 if shift < 0
+            shifts <<  shift
+        end
+        corrected_shifts = get_corrected_shifts(message.length, shifts)
+    end
+
+    def get_corrected_shifts(length, shifts)
+        corrected_shifts = Array.new(4)
+        i = (length - 4) % 4
+        shifts.each do |shift|
+             corrected_shifts[i] = shift
+             i == 3 ? i = 0 : i += 1
+        end
+        corrected_shifts
+    end
+
+    def get_crack_key(shifts, date)
+        offsets = get_offsets(date)
+        keys = []
+        shifts.map.with_index { |shift,i| shift - offsets[i] >= 0 ? keys << shift - offsets[i] : keys << (shift + 27) - offsets[i]}
+        original_keys = keys.dup
+        key_strings = get_key_strings(keys)
+        original_key_strings = key_strings.dup
+        key_strings[1 .. 4].each.with_index do |key_string,i|
+            while key_string[0] != key_strings[i][1]
+                keys[i + 1] += 27
+                if keys[i + 1] >= 100
+                    keys[i] += 27
+                    key_strings[i] = keys[i].to_s
+                    keys[i + 1] = original_keys[i + 1]
+                    key_strings[i + 1] = original_key_strings[i + 1] 
+                end
+                key_string = keys[i + 1].to_s
+            end
+            key_strings[i+1] = key_string
+        end
+        key = key_strings[0] + key_strings[1][-1] + key_strings[2][-1] + key_strings[3][-1]
+    end
+
+    def get_key_strings(keys)
+        key_check = keys.map do |key| 
+            key_string = key.to_s
+            key_string.prepend("0") until key_string.length == 2
+            key_string
+        end
     end
 end
